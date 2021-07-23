f8bd8258c7c8b589dba1173640aa0224ce9a0b03
Muhammed Teuvazhukov <muhammed.teuvazhukov@tibbo.com>
2021-07-24 01:44:22 +0300

    Add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Button, DatePicker, version } from "antd";
 import React from "react";
 
 function App() {
-  return <main className="App">Whatsapp</main>;
+  return (
+    <main className="App">
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+    </main>
+  );
 }
 
 export default App;
